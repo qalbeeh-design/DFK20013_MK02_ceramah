a4b0fe1f227dbabe46d3db40a83153825e6aff9d
qalbeeh-design <ntqalmizi@gmail.com>
2026-01-23 16:53:15 +0800

    add project

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 document.getElementById("tempahanForm").addEventListener("submit", function(e) {
     e.preventDefault();
 
-    const harga = 65;
+    let hargaSeunit = 65;
 
-    let nama= document.getElementById("nama").value;
-    let email= document.getElementById("email").value;
-    let telefon= document.getElementById("telefon").value;
-    let kategori= document.getElementById("kategori").value;
-    let slot= document.getElementById("slot").value;
+    let nama = document.getElementById("nama").value;
+    let email = document.getElementById("email").value;
+    let telefon = document.getElementById("telefon").value;
+    let kategori = document.getElementById("kategori").value;
+    let slot = document.getElementById("slot").value;
 
-    let jumlah= harga * slot;
+    let jumlahYuran = hargaSeunit * slot;
 
-    document.getElementById("output").innerHTML =`
-    <p><b>Nama:</b> ${nama}</p>
-    <p><b>Email:</b> ${email}</p>
-    <p><b>Telefon:</b> ${telefon}</p>
-    <p><b>Kategori Peserta:</b> ${kategori}</p>
-    <p><b>Bilangan Slot:</b> ${slot}</p>
-    <p><b>Jumlah Yuran:</b> RM ${jumlah}</p>
+    let paparan = `
+        <p><b>Nama:</b> ${nama}</p>
+        <p><b>Email:</b> ${email}</p>
+        <p><b>Telefon:</b> ${telefon}</p>
+        <p><b>Kategori Peserta:</b> ${kategori}</p>
+        <p><b>Bilangan Slot:</b> ${slot}</p>
+        <p><b>Jumlah Yuran:</b> RM ${jumlahYuran}</p>
     `;
 
+    document.getElementById("output").innerHTML = paparan;
     document.getElementById("pengesahan").classList.remove("hidden");
 });
